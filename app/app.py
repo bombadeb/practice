9b903d57fec896e8f99624a957f47cddf09e2522
bombadeb <bomba.deb@gmail.com>
2022-07-25 09:45:58 +0530

    Updated for long

--- app/app.py
+++ app/app.py
@@ -24,6 +24,22 @@ def test_short_api(id):
     else:
         return {'Test Result' : 'Not 200 Response'}
 
+@app.get("/testapi/long/{id}")
+def test_long_api(id):
+    response = requests.get("https://ionaapp.com/assignment-magic/dk/short/ab")
+    if response.status_code == 200:
+        if len(id) == 3:
+            json_response = response.json()
+            print(json_response['uid'])
+            if re.fullmatch(r'[0-9a-fA-F]{32}',json_response['uid']):
+                return {'Test Result': 'SUCCESS'}
+            else:
+                return {'Test Result': 'SUCCESS', "Reason": "UID configuration mismatch"}
+        else:
+            return {'Test Result' : 'FAILURE', 'Reason': 'Value should be 3 characters only'}
+    else:
+        return {'Test Result' : 'Not 200 Response'}
+
 
 
 if __name__ == "__main__":
